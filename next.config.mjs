044052e4def5240b34e23adb1a33cbedc4286ffe
Ben <skoghoern@gmail.com>
2025-04-16 13:30:19 +0200

    disabled image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   output: "export", // Re-enable for build
   basePath: "/profile",
   //assetPrefix: "/profile/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
