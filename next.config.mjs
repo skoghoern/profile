5dc7164d07a5838e2e64e3fe1eaccea1da23b010
Ben <skoghoern@gmail.com>
2025-04-16 13:33:23 +0200

    readded asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export", // Re-enable for build
   basePath: "/profile",
-  //assetPrefix: "/profile/",
+  assetPrefix: "/profile/",
   images: {
     unoptimized: true,
   },
